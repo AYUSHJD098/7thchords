658e1db009b232c50df6ea8ee07a4f93ad2a12eb
AYUSHJD098 <66873088+AYUSHJD098@users.noreply.github.com>
2020-08-14 21:08:20 +0530

    helluwafr

--- ag/settings.py
+++ ag/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'n*+y1#6(8h4t7pq**@3v%lng0a$jx114lb+c(4tr*_j+)-r75%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
