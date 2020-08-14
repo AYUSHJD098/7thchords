a87e33b2f892765b566072bf368aec6176f4a7f5
AYUSHJD098 <66873088+AYUSHJD098@users.noreply.github.com>
2020-08-14 23:52:28 +0530

    i am really deakgyfudstear7f

--- ag/settings.py
+++ ag/settings.py
@@ -133,10 +133,3 @@ STATIC_URL = '/static/'
 MEDIA_URL ="/images/"
 
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
