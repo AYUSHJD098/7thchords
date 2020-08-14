0c7dd781512e3a9bcea8a7583742f4b664f49c05
AYUSHJD098 <66873088+AYUSHJD098@users.noreply.github.com>
2020-08-14 23:07:50 +0530

    i am really deakgyfudstear

--- ag/settings.py
+++ ag/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static')
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
-
-MEDIA_URL ="/images/"
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
